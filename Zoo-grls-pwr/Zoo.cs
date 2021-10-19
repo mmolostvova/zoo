210a58e72e64f2b9197b130752ede4c9d79dfe50
DESKTOP-8SKND86\Legion <Faljoy.Kodzy@i.ua>
2021-10-19 19:36:11 +0300

    Add a zoo.cs

---
+++ Zoo-grls-pwr/Zoo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_grls_pwr
+{
+    class Zoo
+    {
+        public int AmauntVisitors = 0;
+        public double ProfitOfDay = 0;
+        public double Treat = 200;
+        public string ZooName;
+        public int MaxVisitors = 5;
+        public List<Animal> Animals;
+        public Zoo(string zooName)
+        {
+            ZooName = zooName;
+
+            Animals = new List<Animal>()
+            {
+                {new Animal("Tiger", 100.00, true)},
+                {new Animal("Snake", 300.00, true)},
+                {new Animal("Spiders", 350.00, true)},
+                {new Animal("Raccon", 200.00, false)},
+                {new Animal("Fox", 150.00, false)},
+                {new Animal("Hippo", 400.00, false)},
+                {new Animal("Penguins", 400.00, false)},
+                {new Animal("Bears", 200.00, false)},
+                {new Animal("Giraffe", 300.00, false)},
+                {new Animal("Monkey", 200.00, false)},
+            };
+        }
+    }
+}
