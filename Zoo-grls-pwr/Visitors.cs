399d0cb28af5fe28a9c76e423472f869a130562a
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-10-19 17:55:00 +0300

    Add Visitors.cs

---
+++ Zoo-grls-pwr/Visitors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_grls_pwr
+{
+    class Visitors
+    {
+        public int Age;
+        public double Money;
+
+        public Visitors(int age)
+        {
+            Age = age;
+            Random rnd = new Random();
+            Money = rnd.Next(1, 5000);
+        }
+    }
+}
