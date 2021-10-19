d8fc09d1c8ba8b01df63de19ffa31982a2f8a414
Marina <mmolostvova@gmail.com>
2021-10-19 16:49:50 +0300

    first commit

---
+++ Zoo-grls-pwr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zoo_grls_pwr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
