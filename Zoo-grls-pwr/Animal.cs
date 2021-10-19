3c15bb63d93241bbfafbd13622ea5ce77c8370ab
HOME-PC\Ket <ket.bodnarchuk@gmail.com>
2021-10-19 17:51:56 +0300

    Add Animal + Delete unnesessory files

---
+++ Zoo-grls-pwr/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zoo_grls_pwr
+{
+    class Animal
+    {
+        public string NameOfAnimal;
+        public double Cost;
+        public bool IsScary;
+
+        public Animal(string nameOfAnimal, double cost, bool isScary)
+        {
+            NameOfAnimal = nameOfAnimal;
+            Cost = cost;
+            IsScary = isScary;
+        }
+    }
+}
