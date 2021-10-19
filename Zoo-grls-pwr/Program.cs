d9a79d88cef7231bc3b0cc3e2996433f0b388fce
Marina <mmolostvova@gmail.com>
2021-10-19 17:53:47 +0300

    Changed content of Program.cs file

--- Zoo-grls-pwr/Program.cs
+++ Zoo-grls-pwr/Program.cs
@@ -6,7 +6,8 @@ namespace Zoo_grls_pwr
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zoo myZoo = new Zoo("Cassablanca");
+            Menu.Start(myZoo);
         }
     }
 }
